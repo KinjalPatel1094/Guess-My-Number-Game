684212c40c3751c6e9b9ec370b99d28fc697304e
Kinjal Patel <64445024+KinjalPatel1094@users.noreply.github.com>
2024-02-19 00:42:19 -0800

    Add files via upload
    First Commit

---
+++ script.js
@@ -0,0 +1,59 @@
+'use strict';
+
+//Generating a random secretnumber
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+let highscore = 0;
+
+const displayMessage = function (message) {
+  document.querySelector('.message').textContent = message;
+};
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  if (!guess) {
+    // document.querySelector('.message').textContent =  'No Number!👎';
+    displayMessage('No Number!👎');
+
+    // When player wins
+  } else if (guess === secretNumber) {
+    displayMessage('🎉 Correct Number!');
+    document.querySelector('.number').textContent = secretNumber;
+
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
+
+    //when guess is wrong
+  } else if (guess !== secretNumber) {
+    //When score goes to 0
+    if (score > 1) {
+      displayMessage(guess > secretNumber ? '📈 Too High!' : '📉 Too Low!');
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      displayMessage('You lost the game!');
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+});
+
+//Reset the game
+document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+  displayMessage('Start Guessing...');
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.guess').value = '';
+
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+});
